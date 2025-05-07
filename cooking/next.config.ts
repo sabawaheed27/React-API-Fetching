b092bb3a8a036645d68f1f7474ecfcaab9f98dda
Saba Waheed <saba_sh27@hotmail.com>
2025-05-07 14:19:27 +0200

    added file

--- cooking/next.config.ts
+++ cooking/next.config.ts
@@ -1,12 +1,7 @@
-import type { NextConfig } from "next";
-
-
-/** @type {import('next').NextConfig} */
-const NextConfig = {
+const nextConfig = {
   images: {
     domains: ["www.themealdb.com"],
   },
 };
 
-module.exports = NextConfig;
-
+export default nextConfig;
